154c3e0ecba760e47af2653cbfdfc847aacb45a4
Will Berg <william.berg08@gmail.com>
2016-10-30 19:16:39 +0000

    Complete implementation

--- src/main/java/nu/mine/wberg/metapack/AccumulatorImpl.java
+++ src/main/java/nu/mine/wberg/metapack/AccumulatorImpl.java
@@ -1,17 +1,30 @@
 package nu.mine.wberg.metapack;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class AccumulatorImpl implements Accumulator {
 
+    private AtomicInteger accumulator;
+
+    public AccumulatorImpl() {
+        accumulator = new AtomicInteger();
+    }
+
     public int accumulate(int... values) {
-        return 0;
+        int acc = 0;
+        for(int a : values) {
+            acc += a;
+        }
+        accumulator.addAndGet(acc);
+        return acc;
     }
 
     public int getTotal() {
-        return 0;
+        return accumulator.get();
     }
 
     public void reset() {
-
+        accumulator = new AtomicInteger();
     }
 
 }
