e042e896ad1e471162d0551fb6b93e1968b6a3d4
Will Berg <william.berg08@gmail.com>
2016-10-30 18:42:57 +0000

    Add non-concurrent tests

--- src/test/java/nu/mine/wberg/metapack/AccumulatorImplTest.java
+++ src/test/java/nu/mine/wberg/metapack/AccumulatorImplTest.java
@@ -1,6 +1,9 @@
 package nu.mine.wberg.metapack;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
@@ -14,4 +17,39 @@ public class AccumulatorImplTest {
         accumulatorImpl = new AccumulatorImpl();
     }
 
+    @Test
+    public void isInitialisedWithZero() {
+        assertEquals(0, accumulatorImpl.getTotal());
+    }
+
+    @Test
+    public void accumulateOne_accumulates() {
+        accumulatorImpl.accumulate(1);
+        assertEquals(1, accumulatorImpl.getTotal());
+    }
+
+    @Test
+    public void accumulateMultiple_returnsSum() {
+        assertEquals(2, accumulatorImpl.accumulate(1,1));
+    }
+
+    @Test
+    public void accumulateMultiple_accumulates() {
+        accumulatorImpl.accumulate(1,1);
+        assertEquals(2, accumulatorImpl.getTotal());
+    }
+
+    @Test
+    public void accumulateSequence_accumulates() {
+        accumulatorImpl.accumulate(1);
+        accumulatorImpl.accumulate(1);
+        assertEquals(2, accumulatorImpl.getTotal());
+    }
+
+    @Test
+    public void reset_resetsToZero() {
+        accumulatorImpl.accumulate(1);
+        accumulatorImpl.reset();
+        assertEquals(0, accumulatorImpl.getTotal());
+    }
 }
